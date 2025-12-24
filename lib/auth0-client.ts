c39da3a7c3013bd2db43e19b4e09a01d3f7d17c2
Kevin Sherman <kevin-sherman-95@users.noreply.github.com>
2025-12-24 14:11:58 -0800

    Fix typo: authorizationParams -> authorizationParameters

--- lib/auth0-client.ts
+++ lib/auth0-client.ts
@@ -6,7 +6,7 @@ export const auth0 = new Auth0Client({
   // The client reads from environment variables automatically:
   // AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_SECRET, APP_BASE_URL
   
-  authorizationParams: {
+  authorizationParameters: {
     // Always show the account picker screen
     prompt: 'select_account',
   },
