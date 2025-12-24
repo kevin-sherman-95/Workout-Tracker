0a9ee5fe48423c93a33fdbfd2959f949a29d05cd
Kevin Sherman <kevin-sherman-95@users.noreply.github.com>
2025-12-24 14:09:53 -0800

    Add prompt: select_account to show account picker on login

--- lib/auth0-client.ts
+++ lib/auth0-client.ts
@@ -5,4 +5,9 @@ import { Auth0Client } from "@auth0/nextjs-auth0/server";
 export const auth0 = new Auth0Client({
   // The client reads from environment variables automatically:
   // AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_SECRET, APP_BASE_URL
+  
+  authorizationParams: {
+    // Always show the account picker screen
+    prompt: 'select_account',
+  },
 });
